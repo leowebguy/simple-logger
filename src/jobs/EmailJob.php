f5451b433226d4102d94145c7ff2f79a47cd384f
Leoncio, Leo <lleoncio@healthfirst.org>
2024-04-01 15:24:51 -0400

    + RetryableJobInterface

--- src/jobs/EmailJob.php
+++ src/jobs/EmailJob.php
@@ -11,14 +11,25 @@ namespace leowebguy\simplelogger\jobs;
 
 use craft\queue\BaseJob;
 use leowebguy\simplelogger\SimpleLogger;
+use yii\queue\RetryableJobInterface;
 
-class EmailJob extends BaseJob
+class EmailJob extends BaseJob implements RetryableJobInterface
 {
     public array $to;
 
     public function defaultDescription(): string
     {
         return 'Sending Simple Logger Report';
+    }
+
+    public function getTtr(): float|int
+    {
+        return 15 * 60;
+    }
+
+    public function canRetry($attempt, $error): bool
+    {
+        return ($attempt < 2) && ($error instanceof TemporaryException);
     }
 
     public function execute($queue): void
